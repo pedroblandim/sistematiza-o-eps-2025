2e4a7b853d02953603a16d67944c9995790f1877
pedroblandim <pedro.blandim@gmail.com>
2025-09-19 20:28:59 -0300

    feat: implement UseCaseConfig for tasks

---
+++ backend/src/main/java/br/com/simplifytec/gamification/tasks/infrastructure/config/UseCaseConfig.java
@@ -0,0 +1,46 @@
+package br.com.simplifytec.gamification.tasks.infrastructure.config;
+
+import br.com.simplifytec.gamification.tasks.application.*;
+import br.com.simplifytec.gamification.tasks.domain.repository.TaskRepository;
+import br.com.simplifytec.gamification.users.domain.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UseCaseConfig {
+
+    @Bean
+    public CreateTaskUseCase createTaskEntryUseCase(TaskRepository repository) {
+        return new CreateTaskUseCase(repository);
+    }
+
+    @Bean
+    public UpdateTaskUseCase updateTaskEntryUseCase(TaskRepository repository) {
+        return new UpdateTaskUseCase(repository);
+    }
+
+    @Bean
+    public ListTasksUseCase listTasksEntryUseCase(TaskRepository repository) {
+        return new ListTasksUseCase(repository);
+    }
+
+    @Bean
+    public ApproveTaskUseCase approveTaskUseCase(TaskRepository taskRepository, UserRepository userRepository) {
+        return new ApproveTaskUseCase(taskRepository, userRepository);
+    }
+
+    @Bean
+    public ListApprovedTasksUseCase listApprovedTasksUseCase(TaskRepository repository) {
+        return new ListApprovedTasksUseCase(repository);
+    }
+
+    @Bean
+    public ListUserTasksUseCase listUserTasksUseCase(TaskRepository repository) {
+        return new ListUserTasksUseCase(repository);
+    }
+
+    @Bean
+    public SubmitTaskForApprovalUseCase submitTaskForApprovalUseCase(TaskRepository taskRepository) {
+        return new SubmitTaskForApprovalUseCase(taskRepository);
+    }
+}
