22b7358379d4f8297f6f35a77e7ae916824f7889
pedroblandim <pedro.blandim@gmail.com>
2025-09-20 00:49:13 -0300

    fix: adjust UserRepository save method

--- backend/src/main/java/br/com/simplifytec/gamification/users/domain/repository/UserRepository.java
+++ backend/src/main/java/br/com/simplifytec/gamification/users/domain/repository/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository {
-    Task save(User task);
+    User save(User user);
     Optional<User> findById(UUID userId);
     Optional<User> findByEmail(String email);
 }
