647245711ba6ffe23f89e789c3370b3e111266df
pedroblandim <pedro.blandim@gmail.com>
2025-09-20 00:47:22 -0300

    refact: rename UseCaseConfig classes

--- backend/src/main/java/br/com/simplifytec/gamification/tasks/infrastructure/config/TasksApplicationConfig.java
+++ backend/src/main/java/br/com/simplifytec/gamification/tasks/infrastructure/config/TasksApplicationConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicationConfig {
+public class TasksApplicationConfig {
 
     @Bean
     public CreateTaskUseCase createTaskEntryUseCase(TaskRepository repository) {
